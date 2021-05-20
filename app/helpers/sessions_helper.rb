36987cb3c5bc92f961dfac23b2a71f5266cb9a1a
hanaonomaru <hanao.n1227@gmail.com>
2021-05-20 21:49:51 +0900

    8syou

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,7 +23,12 @@ module SessionsHelper
                 @current_user = user
             end
         end
-     end
+    end
+
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+        user && user == current_user
+    end
 
 
     # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -43,5 +48,16 @@ module SessionsHelper
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    # 記憶したURLもしくはデフォルト値にリダイレクト
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # アクセスしようとしたURLを覚えておく
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,6 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  # テストユーザーとしてログインする
   def log_in_as(user)
     session[:user_id] = user.id
   end
@@ -20,7 +19,9 @@ class ActionDispatch::IntegrationTest
 
   # テストユーザーとしてログインする
   def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me}}
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 
 end
